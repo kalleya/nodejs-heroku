31a007857e7006ad1261f2c92e77b22c0e21a07f
Kalle Ylä-Anttila <kallenposti@gmail.com>
2013-06-12 21:28:34 +0300

    Make it look better

--- web.js
+++ web.js
@@ -5,15 +5,15 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-  res.render('index.ejs', {
-    title: "anything I want",
-    heading: "hello world",
-    something: "foo bar",
-    layout: false
-  });
+	res.render('index.ejs', {
+		title: "NodeJS @ Heroku",
+		heading: "hello world",
+		something: "Simple NodeJS app running on Heroku.",
+		layout: false
+	});
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
